3d7cdcd51ee172415dfd7625c92c4d82c9285dce
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-07-26 20:04:02 +0700

    [ADMIN] Merge-230

--- src/constants/Paths/Path.js
+++ src/constants/Paths/Path.js
@@ -1,6 +1,7 @@
 export const PATH = {
   INDEX: "/",
   TAG: "/admin/tags",
+  CERTIFICATE: "/admin/certificate",
   MAJOR_GROUP: "/admin/major-group",
   MAJOR: "/admin/major",
 };
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,12 +5,14 @@ import HomePage from "../pages/HomePage";
 import MajorGroupPage from "../pages/MajorGroupPage";
 import MajorPage from "../pages/MajorPage";
 import TagPage from "../pages/TagPage";
+import CertificatePage from "../pages/certificate/CertificatePage";
 const AppRouter = () => (
   <Routes>
     <Route path={PATH.TAG} element={<TagPage />} />
     <Route path={PATH.INDEX} element={<HomePage />} />
     <Route path={PATH.MAJOR_GROUP} element={<MajorGroupPage />} />
     <Route path={PATH.MAJOR} element={<MajorPage />} />
+      <Route path={PATH.CERTIFICATE} element={<CertificatePage />} />
   </Routes>
 );
 export default AppRouter;
